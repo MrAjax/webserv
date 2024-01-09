ae08b4a610f5de2122f260557bd6451e8995f971
Pierre GIRAUDEAU <pgiraude@paul-f4Ar6s4.clusters.42paris.fr>
2024-01-09 14:13:34 +0100

    starting parsing http request

---
+++ webserv_test/HttpRequest.cpp
@@ -0,0 +1,30 @@
+#include "HttpRequest.hpp"
+
+#define MAXLINE 4096
+
+HttpRequest::HttpRequest(void){}
+
+HttpRequest::~HttpRequest(void){}
+
+void    HttpRequest::parsingHeader(int connfd)
+{
+    uint8_t recvline[MAXLINE + 1];
+    memset(recvline, 0, MAXLINE);
+    int n;
+
+    while ((n = read(connfd, recvline, MAXLINE - 1)) > 0)
+	{
+		printf("%s\n", recvline);
+
+        if (recvline )
+
+
+
+		if (recvline[n - 1] == '\n')
+			break;
+		memset(recvline, 0, MAXLINE);
+	}
+    if (n < 0)
+        printf("read error\n");
+}
+
--- webserv_test/web_server.cpp
+++ webserv_test/web_server.cpp
@@ -1,5 +1,35 @@
 #include "web_server.hpp"
+#include "HttpRequest.hpp"
 #include <string.h>
+#include <sstream>
+#include <cstring>
+#include <iostream>
+
+void sendhtml(int connfd)
+{
+	std::ifstream file("index.html");
+
+	if (file.is_open())
+	{
+		std::stringstream ss;
+		ss << file.rdbuf();
+		file.close();
+	
+	std::string htmlcontent = ss.str();
+
+	std::string response;
+	response = "HTTP/1.1 200 OK\r\n";
+	response += "Content-Type: text/html\r\n";
+	response += "Content-Length: " + std::to_string(htmlcontent.length()) + "\r\n\r\n";
+    response += htmlcontent;
+
+	write(connfd, response.c_str(), response.length());
+	}
+	else{
+		std::cerr << "Error HTML" << std::endl;
+	}
+
+}
 
 int main(int argc, char **argv)
 {
@@ -23,29 +53,27 @@ int main(int argc, char **argv)
 		err_n_die("listen error.");
 
     // en gros accepte la connection et la reset a l infini jusqu'a ce qu on en veuille plus
-    for (;;) {
+    for (;;)
+	{
 		struct sockaddr_in	addr;
 		socklen_t			addr_len;
 
 		printf("waiting for a connection on port %d\n", SERVER_PORT);
 		fflush(stdout);
-		//on attend une reponse avec accept
+		//on attend une reponse avec accept, c est une commande bloquante
 		connfd = accept(listenfd, (SA *) NULL, NULL); //NULL car on veut accepter n 'importe quelle connexion
 		memset(recvline, 0, MAXLINE);
 
-        //on read un nb de byte et on print le binaire et le text equivalent
-		while ((n = read(connfd, recvline, MAXLINE - 1)) > 0) {
-			fprintf(stdout, "\n%s\n\n%s", bin2hex(recvline, n), recvline);
-			if (recvline[n - 1] == '\n')
-				break;
-			memset(recvline, 0, MAXLINE);
-		}
-        if (n < 0)
-            err_n_die("read error");
+        //on read un nb de byte et on print --- une version hexa est mise a cote pour check les char non printables bin2hex()
+		
+		HttpRequest Head;
+		Head.parsingHeader(connfd);
 
         // On envoie une reponse en http1.0 200 OK
-        snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\nHello");
-        write(connfd, (char *)buff, strlen((char *)buff));
+        // snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\nHello");
+        // write(connfd, (char *)buff, strlen((char *)buff));
+		printf("connfd->[%d]\n", connfd);
+		sendhtml(connfd);
 		close(connfd);
         
     }
