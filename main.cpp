e1b786b407941f151b323bc1ea2c3cdf6c51cdbf
Pierre GIRAUDEAU <pgiraude@paul-f5Ar3s10.clusters.42paris.fr>
2024-01-27 17:19:48 +0100

    adding servers vector in request

--- main.cpp
+++ main.cpp
@@ -189,8 +189,7 @@ int main(int ac, char **av)
 							//std::cout << "server name: " << it->second->getServerName() << "\n";
 							server_log("other request on clientFD", DEBUG);
 							int status = clientMap[pollfds[i].fd].second->processingRequest();
-							std::cout << RED "PATH REQUEST= " << clientMap[pollfds[i].fd].second->getPath() << RESET << std::endl;
-							std::cout << RED "PATH METHODE= " << clientMap[pollfds[i].fd].second->getMethod() << RESET << std::endl;
+							clientMap[pollfds[i].fd].second->printAttribute();
 							if (status == DONE_ALL)
 							{
 								send_response(pollfds[i].fd, *it->second, *clientMap[pollfds[i].fd].second);
