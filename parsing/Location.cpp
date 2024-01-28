d955a591b699f967b425903a7b15cbed7918a03c
Bat <baptiste.hommeril@gmail.com>
2024-01-28 17:23:19 +0100

    handl some more on location.cpp

--- parsing/Location.cpp
+++ parsing/Location.cpp
@@ -1,10 +1,12 @@
 #include "Location.hpp"
+#include "../utils/utils.hpp"
 
-Location::Location(std::vector<std::string> config) 
+Location::Location(std::vector<std::string> config) : _autoindex(false) 
 {
 	void (Location::*ptr[PARAM_LOC])(std::string const&) =
-		{&Location::p_allow_methods, &Location::p_root };
-	std::string keyword[] = {"allow_methods", "root"};
+		{&Location::p_allow_methods, &Location::p_httpRedirection, &Location::p_root, &Location::p_autoindex,
+			&Location::p_index	};
+	std::string keyword[] = {"allow_methods", "return", "root", "autoindex", "index"};
 
 	for (size_t i = 0; i < config.size() ; i++) {
 	//	std::cout << "line recorded = [" << config[i] << "]" << std::endl;
@@ -46,11 +48,49 @@ void Location::p_allow_methods(std::string const& line) {
 		else
 			_allow_methods.push_back(line.substr(pos, end - pos));
 		pos = end;	
-	}		
-/*	for(size_t i = 0; i < _allow_methods.size(); ++i) {
+	}
+
+		for(size_t i = 0; i < _allow_methods.size(); ++i) {
 		std::cout << "[" << _allow_methods[i] << "]}";
 	}	
-	std::cout << "END" << std::endl;*/
+	std::cout << "END" << std::endl;
+}	
+
+void Location::p_httpRedirection(std::string const& line) {
+
+	size_t pos = std::string("return").length();
+	while (pos < line.length() && std::isspace(line[pos])) {
+		++pos;
+	}
+	_return = line.substr(pos, line.length() - pos);
+	std::cout << "http redirection = " << _return << std::endl;
+}	
+
+void Location::p_autoindex(std::string const& line ) {
+
+	size_t pos = std::string("autoindex").length();
+	while (pos < line.length() && std::isspace(line[pos])) {
+		++pos;
+	}
+	std::string temp = line.substr(pos, line.length() - pos);
+	std::cout << "temp [" << temp << "]" << std::endl;
+	if (temp == "on")
+		_autoindex = true;
+	else if (temp == "off")
+		_autoindex = false;
+	else
+		throw error_throw("invalid value " + temp + "in autoindex directive - config file", false);
+	std::cout << "autoindex = " << _autoindex << std::endl;
+}	
+
+void Location::p_index(std::string const& line) {
+
+	size_t pos = std::string("index").length();
+	while (pos < line.length() && std::isspace(line[pos])) {
+		++pos;
+	}
+	_index = line.substr(pos, line.length() - pos);
+	std::cout << "index = " << _index << std::endl;
 }	
 
 std::vector<std::string> Location::getallow_methods(void) const {
@@ -60,3 +100,15 @@ std::vector<std::string> Location::getallow_methods(void) const {
 std::string Location::getRoot(void) const {
 	return _root;
 }
+
+std::string Location::getReturn(void) const {
+	return _return;
+}	
+
+bool	Location::getAutoindex(void) const {
+	return _autoindex;
+}	
+
+std::string Location::getIndex(void) const {
+	return _index;
+}	
--- parsing/Location.hpp
+++ parsing/Location.hpp
@@ -3,7 +3,7 @@
 
 #include "../inc/webserv.hpp"
 
-#define PARAM_LOC 2
+#define PARAM_LOC 5
 typedef void (*FuncPtr)(std::string const&);
 
 class Location {
@@ -14,14 +14,24 @@ public:
 	~Location(void);
 
 	std::string getRoot( void ) const;
+	std::string getReturn( void ) const;
+	std::string getIndex( void ) const;
 	std::vector<std::string> getallow_methods( void ) const;
+	bool	getAutoindex( void ) const;
 
 private:
 
 	void	p_root(std::string const& line);
 	void	p_allow_methods(std::string const& line);
+	void	p_httpRedirection(std::string const& line);
+	void	p_autoindex(std::string const& line);
+	void	p_index(std::string const& line);
+
 	std::string _root;
+	std::string _return;
+	std::string _index;
 	std::vector<std::string> _allow_methods;
+	bool	_autoindex;
 };
 
 #endif
