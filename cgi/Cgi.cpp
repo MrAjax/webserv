a4ce2aa22b2444821507561760a49cf61b14c852
opaleboreale <opaleboreale@protonmail.com>
2024-02-02 16:03:13 +0100

    cgi

--- cgi/Cgi.cpp
+++ cgi/Cgi.cpp
@@ -1,11 +1,12 @@
 #include "Cgi.hpp"
 
+extern char **environ;
+
 Cgi::Cgi() {}
 
 Cgi::~Cgi() {}
 
 int	Cgi::exec_cgi(std::string path) {
-	int	fd[2];
 	if (access(path.c_str(), F_OK) < 0) {
 		server_log("Cgi file not found:" + path, DEBUG);
 		return (404);
@@ -14,11 +15,24 @@ int	Cgi::exec_cgi(std::string path) {
 		server_log("Cannot execute cgi file:" + path, DEBUG);
 		return(403);	
 	}
-	if (pipe(fd) < 0) {
-		server_log("Pipe error in cgi", DEBUG);
-		return 500;
+	int pid = fork();
+	if (pid < 0) {
+		server_log("Fork error in cgi", DEBUG);
+		return (500);
 	}
-	close(fd[0]);
-	close(fd[1]);
-	return 0;
+	else if (pid == 0) {
+		char	*arg[] = {const_cast<char*>(path.c_str()), NULL};
+		execve(path.c_str(), arg, environ);
+	}
+	else {
+		int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+            return 200;
+		else {
+            server_log("CGI script execution failed: " + path, ERROR);
+            return 500;
+        }
+	}
+	return 200;
 }
--- method/Get.cpp
+++ method/Get.cpp
@@ -4,26 +4,12 @@ Get::Get(std::string path, std::string content): Method(path, content) {}
 
 Get::~Get() {}
 
-static	bool	is_cgi(std::string file_name) {
-	std::string	type;
-
-	type = ContentTypeFinder::get_content_type(file_name);
-	if (type != "cgi")
-		return (false);
-	return (true);
-}
-
 void	Get::execute_method(Server &serv) {
 	std::string			file_name(this->get_path());
 	std::ifstream		file_requested(file_name.c_str());
 	std::stringstream	file_content;
 
-	if (is_cgi(file_name)) {
-		server_log("Found CGI", DEBUG);
-		file_name.resize(file_name.size() - EXT_SIZE);
-		set_statuscode(Cgi::exec_cgi(file_name));
-	}
-	else if (!file_requested.is_open()) {
+	if (!file_requested.is_open()) {
 		server_log("Cannot open " + file_name, ERROR);
 		throw	StatusSender::send_status(404, serv);
 	}
