b069c6b75409765f4d18b407dcbbc9ba2402ac3a
opaleboreale <opaleboreale@protonmail.com>
2024-02-14 19:16:38 +0100

    fixed error 404 for directories

--- method/Delete.hpp
+++ method/Delete.hpp
@@ -9,7 +9,6 @@ class Delete: public Method {
 	Delete(std::string path, std::string content, std::string connection_status);
 	~Delete();
 	void	remove_file(Server &serv);
-	void	remove_line(std::string arg, Server &serv);
 	void	execute_method(Server &serv);
 };
 
--- utils/HttpStatusCode.hpp
+++ utils/HttpStatusCode.hpp
@@ -4,8 +4,10 @@
 #include "../inc/webserv.hpp"
 
 class HttpStatusCode {
-	std::map<int, std::string>	_http_status_code;
-	std::string					_http_error_list;
+	void	init_class();
+	static	std::map<int, std::string>	_http_status_code;
+	static	std::string					_http_error_list;
+	static	bool						_initialized;
 	HttpStatusCode();
 	~HttpStatusCode();
 	std::map<int, std::string>	&_get_http_status_code();
